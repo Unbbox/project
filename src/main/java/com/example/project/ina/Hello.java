3ffda3eedd6770c0e757bff635df8c254ef017fa
2naaayoon <inayoon78@gmail.com>
2024-05-08 14:25:04 +0900

    Hello 작성

---
+++ src/main/java/com/example/project/ina/Hello.java
@@ -0,0 +1,8 @@
+package com.example.project.ina;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요");
+        System.out.println("반갑습니다~");
+    }
+}
